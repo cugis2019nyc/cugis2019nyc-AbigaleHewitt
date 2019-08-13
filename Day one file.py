47a24337de2c631ac8bb80ccc82ce003520a58de
AbigaleHewitt <54039973+AbigaleHewitt@users.noreply.github.com>
2019-08-13 13:43:28 -0400

    Add files via upload

---
+++ Day one file.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")  #greeting
+print("19")  #number
+print(5*2)
+print(5/2)
+print(5**2)
+print(8/9*3)
+def add(a,b):
+    add =a+b
+    print(add)
+
+add(2,3)
+
+add (419,1)
+
+def add(a,b,c):
+    add = a+b+c
+    print ("sum of ",a,b ,"and",c,"is", add)
+    print("sum of 4387.798 and 25970.56 is..." )
+
+
+add (4387.798,25970.56,789)
+print("sum of 4387.798 and 25970.56 is..." )
+
+
+    
+ 
+def area(b,h):
+    area = 1/2*b*h
+    print("area of a traingle with base",b,"and height",h, "is", area)
+    
+    
+    area (2,3)
+
+area (4,5)
